f5a66307788f86e538a12daf35655b788fefd0b1
Kilian Ciuffolo <385716+kilianc@users.noreply.github.com>
2024-05-12 00:16:19 -0700

    fix: run on pull requests (#2)

--- update-code-coverage-comment.js
+++ update-code-coverage-comment.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const updateCodeCoverageComment = module.exports = async ({ context, github }) => {
+const updateCodeCoverageComment = module.exports = async ({ context, github }, revision) => {
   const comments = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -14,8 +14,8 @@ const updateCodeCoverageComment = module.exports = async ({ context, github }) =
 
   const commentBody = [
     '<!-- coverage -->',
-    `### [Code Coverage Report 🔗](https://kilianc.github.io/base-golang-cli/${process.env.REVISION}.html#file0) for ${process.env.REVISION}`,
-  ].join('\n')
+    `### [Code Coverage Report 🔗](https://${context.repo.owner}.github.io/${context.repo.repo}/${revision}.html#file0) for ${revision}`,
+  ]
 
   if (fs.existsSync('cover.txt') === true) {
     const coverageText = fs.readFileSync('cover.txt', 'utf8').split('\n').slice(0, -1)
@@ -40,7 +40,7 @@ const updateCodeCoverageComment = module.exports = async ({ context, github }) =
     repo: context.repo.repo,
     issue_number: context.issue.number,
     comment_id: coverageComment.id,
-    body: commentBody
+    body: commentBody.join('\n')
   }
 
   if (coverageComment.id) {
