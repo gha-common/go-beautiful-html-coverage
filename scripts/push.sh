3a296d974040fa690fdf632a3e2935fff68a278e
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2024-05-17 17:04:09 -0700

    chore: remove unused tags at ci time

--- scripts/push.sh
+++ scripts/push.sh
@@ -37,10 +37,12 @@ cp "${GITHUB_ACTION_PATH}"/assets/* .
 hash=$(cat index.css index.js | md5sum | awk '{print $1}')
 
 for file in "revisions/${REVISION}.html" "revisions/${REVISION}-inc.html"; do
-  ex -sc '%s/<\/style>/<\/style>\r\t\t<script src="..\/index.js?'"${hash}"'"><\/script>/' -c 'x' "${file}"
+  ex -sc '%s/\n\t\t<style>\_.\{-}<\/style>//' -c 'x' "${file}"
+  ex -sc '%s/\n\t<script>\_.\{-}<\/script>//' -c 'x' "${file}"
   ex -sc '%s/<title>/<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" \/>\r\t\t<title>/' -c 'x' "${file}"
   ex -sc '%s/<title>/<meta http-equiv="Pragma" content="no-cache" \/>\r\t\t<title>/' -c 'x' "${file}"
   ex -sc '%s/<title>/<meta http-equiv="Expires" content="0" \/>\r\t\t<title>/' -c 'x' "${file}"
+  ex -sc '%s/<\/title>/<\/title>\r\t\t<script src="..\/index.js?'"${hash}"'"><\/script>/' -c 'x' "${file}"
 done
 
 # if we are on the main branch, copy files to main.*
