e1b907e806b37bbdf726996f64f61550d3776c82
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2024-05-19 17:15:12 -0700

    chore: fix coverage colors for same line statements

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,7 @@ let loading = load([
   "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js",
   "../index.css?" + document.querySelector('script[src*="index.js"]').src.split('?').pop(),
 ]);
 
@@ -26,18 +27,54 @@ function main() {
     return
   }
 
-  configureIncrementalButton()
-  configureSelectFix()
-  configureTheme()
+  // layout
+  configureFileSelect()
+  addIncrementalButton()
+  addThemeButton()
+
   configureCodeBlocks()
-  highlight('.code .editor')
+  configureSyntaxHighlight('pre .code .editor')
+  addCoverageSpans('pre .coverage span')
   addLineNumbers()
 
   // setup complete, restore the page visibility
   document.documentElement.style.setProperty('opacity', '1')
 }
 
-function configureIncrementalButton() {
+function addCoverageSpans(cssSelector) {
+  let spans = Array.from(document.querySelectorAll(cssSelector))
+
+  spans.forEach((span) => {
+    let html = span.innerHTML
+    let lines = html.split('\n')
+    let covClass = span.classList[0] === 'cov0' ? 'cov-uncovered' : 'cov-covered'
+
+    for (let i = 0; i < lines.length; i++) {
+      let trimmed = lines[i].trim()
+      let [start, end] = lines[i].split(trimmed)
+
+      if (trimmed[0] === '{' || trimmed[0] === '}') {
+        trimmed = trimmed.slice(1).trim()
+        start = lines[i].replace(trimmed, '')
+      }
+
+      if (trimmed[trimmed.length - 1] === '{' || trimmed[trimmed.length - 1] === '}') {
+        trimmed = trimmed.slice(0, -1).trim()
+        end = lines[i].split(trimmed)[1]
+      }
+
+      if (trimmed === '') {
+        lines[i] = `${start || ''}${trimmed}${end || ''}`
+      } else {
+        lines[i] = `${start || ''}<span class="cov ${covClass}">${trimmed}</span>${end || ''}`
+      }
+    }
+
+    span.innerHTML = lines.join('\n')
+  })
+}
+
+function addIncrementalButton() {
   let url = window.location.href
   let isInc = url.includes('-inc.html')
 
@@ -62,7 +99,7 @@ function configureIncrementalButton() {
   document.querySelector('#topbar').appendChild(link)
 }
 
-function configureSelectFix() {
+function configureFileSelect() {
   let files = document.getElementById('files')
 
   files.addEventListener('change', (e) => {
@@ -74,7 +111,7 @@ function configureSelectFix() {
   files.dispatchEvent(new Event('change'))
 }
 
-function configureTheme() {
+function addThemeButton() {
   let isDark = localStorage.getItem('dark') === 'true'
 
   let switchInput = document.createElement('input')
@@ -113,24 +150,23 @@ function toggleDarkMode() {
 }
 
 function configureCodeBlocks() {
-  let pres = Array.from(document.querySelectorAll('#content pre'))
-
-  pres.forEach((pre) => {
+  document.querySelectorAll('#content pre').forEach((pre) => {
     let gutter = document.createElement('div')
     gutter.classList.add('gutter')
 
     let editor = document.createElement('div')
-    editor.classList.add('editor')
-    editor.innerHTML = pre.innerHTML
+    editor.classList.add('editor', 'language-go')
+    editor.innerHTML = pre.innerHTML.replaceAll('    ', '  ')
 
     let code = document.createElement('div')
-    code.classList = 'code'
     code.appendChild(gutter)
     code.appendChild(editor)
 
     let coverage = code.cloneNode(true)
     coverage.classList = 'coverage'
 
+    editor.innerHTML = editor.textContent
+    code.classList = 'code'
     code.style.setProperty('position', 'absolute')
     code.style.setProperty('top', '0')
     code.style.setProperty('left', '0')
@@ -141,49 +177,73 @@ function configureCodeBlocks() {
   })
 }
 
-function highlight(cssSelector) {
+function configureSyntaxHighlight(cssSelector) {
   hljs.configure({ cssSelector, ignoreUnescapedHTML: true })
   hljs.highlightAll()
 }
 
 function addLineNumbers() {
-  let containers = Array.from(document.querySelectorAll('#content pre > div'))
+  let code = document.querySelector('#content pre > div.coverage')
+  let gutter = code.querySelector('.gutter')
+  let editor = code.querySelector('.editor')
+  let lines = editor.innerHTML.split('\n')
+  let gutterHtml = ''
 
-  containers.forEach((container) => {
-    let gutter = container.querySelector('.gutter')
-    let editor = container.querySelector('.editor')
-    let code = editor.innerHTML.replaceAll('    ', '  ')
-    let lines = code.split('\n')
-    let linesCount = lines.length
-    let gutterHtml = ''
+  // this function has two goals:
+  // 1. add line numbers to the gutter
+  // 2. assign a color to the line number based on the coverage of the line
+  //
+  // first, we add a .line-start span to each line in the editor.
+  // this allows us to group the spans in the editor by line.
+  // if a line has only one span, we assign the color of the span to the line number in the gutter.
+  // if a line has more than one span and they have different background colors,
+  // we can assume that the line has multiple statements with multiple coverage states
+  // and we assign a yellow-ish color to the line number in the gutter.
 
-    editor.innerHTML = lines
-      .map((line) => `<span class="line-start"></span>${line}`)
-      .join('\n')
+  editor.innerHTML = lines
+    .map((line) => `<span class="line-start"></span>${line}`)
+    .join('\n')
 
-    let lineStarts = Array.from(editor.querySelectorAll('.line-start'))
+  let lineNumber = 1
+  let spansInLine
+  let spans = Array.from(editor.querySelectorAll('span'))
 
-    for (let i = 0; i < linesCount; i++) {
-      let backgroundColor = window.getComputedStyle(
-        lineStarts[i].parentElement,
-      ).backgroundColor
-      let textColor = backgroundColor.replace(' / 0.1', ' / 1')
+  for (let i = 0; i < spans.length; i++) {
+    let currentSpan = spans[i]
+    let nextSpan = spans[i + 1]
 
-      if (textColor === 'rgba(0, 0, 0, 0)') {
-        gutterHtml += `<div>${i + 1}</div>`
-      } else {
-        gutterHtml += `<div style="background-color: ${backgroundColor}; color: ${textColor};">${i + 1}</div>`
-      }
+    if (currentSpan.classList.contains('line-start')) {
+      spansInLine = []
     }
 
-    gutter.innerHTML = gutterHtml
-  })
+    if (nextSpan?.classList?.contains('cov')) {
+      spansInLine.push(nextSpan)
+      continue
+    }
+
+    if (!nextSpan?.classList.contains('line-start')) {
+      continue
+    }
+
+    let classes = new Set(spansInLine.map((el) => el.classList[1]))
+    let className = classes.size > 1 ? 'cov-mixed' : classes.values().next().value || ''
+
+    gutterHtml += `<div class="ln ${className}">${lineNumber}</div>`
+
+    lineNumber++
+  }
+
+  gutterHtml += `<div class="ln">${lineNumber}</div>`
+  gutter.innerHTML = gutterHtml
+
+  // add line numbers to the coverage gutter
+  document.querySelector('#content pre > div.code > .gutter').innerHTML = gutterHtml
 }
 
 function loadScript(src, state) {
   let script = document.createElement('script')
   script.src = src
-  script.async = true
+  script.async = false
   script.onload = () => {
     console.info(`loaded: ${src}`)
     state.loaded++
