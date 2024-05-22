17d7b5c9356c6482ab6307168d5794a0253d9951
Kilian Ciuffolo <385716+kilianc@users.noreply.github.com>
2024-05-21 22:10:46 -0700

    fix: revert scoll but workaround and use `document.body.scrollTop` (#54)

--- assets/index.js
+++ assets/index.js
@@ -27,11 +27,12 @@ function main() {
     return
   }
 
-  // layout
+  // setup the layout
   configureFileSelect()
   addIncrementalButton()
   addThemeButton()
 
+  // setup the content
   configureCodeBlocks()
   configureSyntaxHighlight('pre .code .editor')
   addCoverageSpans('pre .coverage .editor')
@@ -113,16 +114,11 @@ function configureFileSelect() {
     }
 
     document.location.hash = e.target.value
+
+    document.body.scrollTop = 0;
+
     document.querySelectorAll('.file').forEach((el) => (el.style.display = 'none'))
     el.style.display = 'block'
-
-    let scrollUp = (times) => {
-      if (window.scrollY === 0 && times > 3) return
-      window.scrollTo(0, 0)
-      window.requestAnimationFrame(() => scrollUp(times + 1))
-    }
-
-    scrollUp(1)
   })
 
   files.value = selected
